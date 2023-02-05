a53000cb4b4e4ece2d0114dd2dbfce9119d13db4
Nagitch <kusanagi.y@mujina-prod.com>
2023-02-06 01:22:34 +0900

    wip fixed base info

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,15 +2,15 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
-  lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  domain: import.meta.env.a ?? 'urara-demo.netlify.app',
+  title: 'Mujina Production',
+  subtitle: 'by Nagitch',
+  lang: 'ja-JP',
+  description: 'Mujina Production - Nagitch\'s carrer blanding site',
   author: {
     avatar: '/assets/maskable@512.png',
     name: 'John Doe',
-    status: '🌸',
+    status: '🐺',
     bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
   themeColor: '#3D4451'
