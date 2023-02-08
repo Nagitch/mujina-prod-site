179c249640fde68036f66c979e46fed91f129753
Nagitch <kusanagi.y@mujina-prod.com>
2023-02-09 00:09:41 +0900

    modified basic information

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.a ?? 'urara-demo.netlify.app',
-  title: 'Mujina Production',
-  subtitle: 'by Nagitch',
+  title: '狢制作',
+  subtitle: 'Mujina Production by Nagitch',
   lang: 'ja-JP',
   description: 'Mujina Production - Nagitch\'s carrer blanding site',
   author: {
     avatar: '/assets/maskable@512.png',
     name: 'Nagitch',
     status: '🫠',
-    bio: '個人制作の計画を練りつつ、会社員とフリーランサーを兼業しているプログラマーです。<br />知らないうちに私の書いたコードが北極に保存されてました。。🐻‍❄️'
+    bio: 'ちびちびと個人制作しつつ、会社員とフリーランサーを兼業しているプログラマーです🐺<br><strong>狢制作 (Mujina Production)</strong> は屋号、ブランド名、サークル名 etc... を兼ねたものです。'
   },
   themeColor: '#3D4451'
 }
